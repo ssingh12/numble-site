d5b5c275f90b850b546dd1f89b194b5a1df85a29
Samit Singh <ssingh12@yahoo.com>
2026-08-22 21:27:19 +0100

    Publish from numble@060643a

--- play/app/flutter_bootstrap.js
+++ play/app/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "965966420" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1703645062" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
